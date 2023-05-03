28626a210fc619397dcf61a9b0c63fae2d2e9539
Julia Makulec <s213041@dtu.dk>
2023-05-03 20:39:45 +0200

    Functional vibration screen

--- lib/ui/rpui_vibration_test_body.dart
+++ lib/ui/rpui_vibration_test_body.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:neuro_planner/ui/widgets/vibration_button.dart';
 import 'package:neuro_planner/utils/spacing.dart';
 import 'package:neuro_planner/utils/themes/text_styles.dart';
-import 'package:research_package/model.dart';
 import 'package:research_package/research_package.dart';
+import 'package:vibration/vibration.dart';
+import 'package:async/async.dart';
 
 import '../step/steps/rp_vibration_step.dart';
+import '../utils/themes/styles.dart';
 
 class RPUIVibrationStep extends StatefulWidget {
   final RPVibrationStep vibrationStep;
-
   RPUIVibrationStep(this.vibrationStep);
 
   @override
@@ -44,7 +46,6 @@ class RPUIVibrationStepState extends State<RPUIVibrationStep>
 
   @override
   Widget build(BuildContext context) {
-    print(widget.vibrationStep.answerFormat);
     // List<Widget> answers =
     //     (widget.vibrationStep.answerFormat as RPChoiceAnswerFormat)
     //         .choices
@@ -52,57 +53,50 @@ class RPUIVibrationStepState extends State<RPUIVibrationStep>
     //         .toList();
     return Scaffold(
       body: Center(
-        child: Expanded(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                child: Image.asset(
-                    widget.vibrationStep.vibrationSection.imagePath,
-                    fit: BoxFit.cover),
-              ),
-              verticalSpacing(8),
-              Text(widget.vibrationStep.title,
-                  style: ThemeTextStyle.headline24sp),
-              verticalSpacing(16),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Text(
-                  widget.vibrationStep.text ?? '',
-                  style: ThemeTextStyle.regularIBM18sp,
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              verticalSpacing(24),
-              ElevatedButton(
-                onPressed: () {
-                  //Vibration.vibrate(duration: 30000);
-                },
-                child: const Text('Start vibration'),
-              ),
-              verticalSpacing(24),
-              Text(
-                'Can you feel the vibration?',
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              child: Image.asset(
+                  widget.vibrationStep.vibrationSection.imagePath,
+                  fit: BoxFit.cover),
+            ),
+            verticalSpacing(8),
+            Text(widget.vibrationStep.title,
+                style: ThemeTextStyle.headline24sp),
+            verticalSpacing(16),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                widget.vibrationStep.text ?? '',
                 style: ThemeTextStyle.regularIBM18sp,
                 textAlign: TextAlign.center,
-              ), // TODO change to processing locale
-              Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: RPUIChoiceQuestionBody(
-                      (widget.vibrationStep.answerFormat
-                          as RPChoiceAnswerFormat), (result) {
-                    currentQuestionBodyResult = result;
-                  })
-                  // TODO toggle buttons, move to separate widget component
-                  //   child: ToggleButtons(
-                  //     direction: Axis.horizontal,
-                  //     onPressed: (index) {},
-                  //     children: answers,
-                  //     isSelected: List.generate(answers.length, (index) => false),
-                  //   ),
-                  )
-            ],
-          ),
+              ),
+            ),
+            verticalSpacing(24),
+            const VibrationButton(),
+            verticalSpacing(24),
+            Text(
+              'Can you feel the vibration?',
+              style: ThemeTextStyle.regularIBM18sp,
+              textAlign: TextAlign.center,
+            ), // TODO: change to processing locale
+            Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: RPUIChoiceQuestionBody(
+                    (widget.vibrationStep.answerFormat as RPChoiceAnswerFormat),
+                    (result) {
+                  currentQuestionBodyResult = result;
+                })
+                // TODO: toggle buttons, move to separate widget component
+                //   child: ToggleButtons(
+                //     direction: Axis.horizontal,
+                //     onPressed: (index) {},
+                //     children: answers,
+                //     isSelected: List.generate(answers.length, (index) => false),
+                //   ),
+                )
+          ],
         ),
       ),
     );
