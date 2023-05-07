da40c35cb0f870f0e2803bc080e0cc607e67e8f1
Julia Makulec <s213041@dtu.dk>
2023-05-07 20:44:56 +0200

    Welcome screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:neuro_planner/utils/spacing.dart';
+import 'package:neuro_planner/utils/themes/text_styles.dart';
 import 'examination_page.dart';
 
 void main() {
@@ -54,7 +56,7 @@ class MyApp extends StatelessWidget {
           onSurface: Colors.black,
         ),
       ),
-      home: const MyHomePage(title: 'Vibration Trigger App'),
+      home: const MyHomePage(title: 'Neuropathy assesment'),
     );
   }
 }
@@ -76,16 +78,48 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              child: Text('Begin new examination'),
-              onPressed: () => Navigator.of(context).push(
-                  MaterialPageRoute<dynamic>(
-                      builder: (context) => ExaminationPage())),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 80),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Welcome!',
+                style: ThemeTextStyle.headline24sp.copyWith(
+                  height: 1.25,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              verticalSpacing(48),
+              Text(
+                'You do not have any completed examinations yet.',
+                style: ThemeTextStyle.headline24sp.copyWith(
+                  height: 1.25,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              verticalSpacing(48),
+              GestureDetector(
+                onTap: () => Navigator.of(context).push(
+                    MaterialPageRoute<dynamic>(
+                        builder: (context) => ExaminationPage())),
+                child: Text(
+                  'Tap here to start a new one',
+                  style: ThemeTextStyle.headline24sp.copyWith(
+                    height: 1.25,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              verticalSpacing(24),
+              FloatingActionButton(
+                child: const Icon(Icons.add_rounded, size: 36),
+                onPressed: () => Navigator.of(context).push(
+                    MaterialPageRoute<dynamic>(
+                        builder: (context) => ExaminationPage())),
+              ),
+            ],
+          ),
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
