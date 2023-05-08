bf534f97e6bc295afa3006d1d9b7efdc342f68f1
s174508 <s174508@student.dtu.dk>
2023-05-08 10:54:40 +0200

    call sendReadyToProceed(true) so the BLoC doesn't just use the current value

--- lib/ui/rpui_instruction_step.dart
+++ lib/ui/rpui_instruction_step.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:neuro_planner/step/steps/rp_instruction_step.dart';
 import 'package:neuro_planner/utils/themes/text_styles.dart';
+import 'package:research_package/research_package.dart';
 
 class RPUIInstructionStepWithChildren extends StatelessWidget {
   final RPInstructionStepWithChildren step;
@@ -9,6 +10,7 @@ class RPUIInstructionStepWithChildren extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    blocQuestion.sendReadyToProceed(true);
     return Material(
       textStyle: ThemeTextStyle.headline24sp,
       child: Padding(
