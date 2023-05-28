6d4155762c49c13a70cb9ca6225389bba6b1f0ca
s174508 <s174508@student.dtu.dk>
2023-05-28 21:36:59 +0200

    Danish today and yesterday. Reverted motor change (previous was correct)

--- lib/survey/motor_part.dart
+++ lib/survey/motor_part.dart
@@ -38,8 +38,8 @@ RPInstructionStepWithChildren motorInstructionStep =
 );
 
 List<RPChoice> motorYesNo = [
-  RPChoice(text: 'common.yes', value: 0),
-  RPChoice(text: 'common.no', value: 1),
+  RPChoice(text: 'common.yes', value: 1),
+  RPChoice(text: 'common.no', value: 0),
 ];
 RPChoiceAnswerFormat motorYesNoFormat = RPChoiceAnswerFormat(
     answerStyle: RPChoiceAnswerStyle.SingleChoice, choices: motorYesNo);
