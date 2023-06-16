960d915bd182b577657931c2ead22eabbd4d4c04
Julia Makulec <s213041@dtu.dk>
2023-06-16 18:03:19 +0200

    adding custom icons from figma replacing icons in main page

--- lib/ui/main_page_examinations.dart
+++ lib/ui/main_page_examinations.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:neuro_planner/ui/widgets/neuropathy_icons.dart';
+
 import 'package:neuro_planner/utils/date_formatter.dart';
 import 'package:neuro_planner/utils/generate_csv.dart';
 import 'package:research_package/research_package.dart';
@@ -28,14 +30,14 @@ class MainPageBodyWithExaminations extends StatelessWidget {
             title: DateFormatter(dateTime: taskResults[index].startDate!),
             subtitle: Text(calculateScore(taskResults[index]).toString()),
             leading: Icon(
-              Icons.thermostat,
-              size: 30,
+              NeuropathyIcons.carbon_result,
+              size: 45,
               color: Theme.of(context).colorScheme.primary,
             ),
             trailing: GestureDetector(
               child: Icon(
-                Icons.download,
-                size: 30,
+                Icons.file_download_outlined,
+                size: 35,
                 color: Theme.of(context).colorScheme.primary,
               ),
               onTap: () {
