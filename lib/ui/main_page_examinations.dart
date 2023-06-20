81ea7c587ab918c89bcdad853543fab119e4e36e
Julia Makulec <s213041@dtu.dk>
2023-06-20 13:16:52 +0200

    update to master fix

--- lib/ui/main_page_examinations.dart
+++ lib/ui/main_page_examinations.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:neuro_planner/ui/results/result_page.dart';
 import 'package:neuro_planner/ui/widgets/neuropathy_icons.dart';
 
 import 'package:neuro_planner/utils/date_formatter.dart';
