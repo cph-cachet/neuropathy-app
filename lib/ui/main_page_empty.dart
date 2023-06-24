9ea8a971c73cfb242c10946980cb8d1b3e65203f
s174508 <s174508@student.dtu.dk>
2023-06-24 20:32:26 +0200

    Put GestureDetector back in and add sliding animation

--- lib/ui/main_page_empty.dart
+++ lib/ui/main_page_empty.dart
@@ -9,6 +9,16 @@ import 'package:neuropathy_grading_tool/ui/widgets/add_examination_button.dart';
 class MainPageEmptyResults extends StatelessWidget {
   const MainPageEmptyResults({Key? key}) : super(key: key);
 
+  Widget slideTransition(animation, child) {
+    return SlideTransition(
+      position: Tween<Offset>(
+        begin: const Offset(0, 1),
+        end: Offset.zero,
+      ).chain(CurveTween(curve: Curves.ease)).animate(animation),
+      child: child,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -33,12 +43,20 @@ class MainPageEmptyResults extends StatelessWidget {
               textAlign: TextAlign.center,
             ),
             verticalSpacing(48),
-            Text(
-              Languages.of(context)!.translate('welcome-screen.tap-to-start'),
-              style: ThemeTextStyle.headline24sp.copyWith(
-                height: 1.25,
+            GestureDetector(
+              onTap: () => Navigator.of(context).push(PageRouteBuilder(
+                pageBuilder: (_, __, ___) => ExaminationPage(),
+                transitionDuration: const Duration(milliseconds: 350),
+                transitionsBuilder: (_, animation, __, child) =>
+                    slideTransition(animation, child),
+              )),
+              child: Text(
+                Languages.of(context)!.translate('welcome-screen.tap-to-start'),
+                style: ThemeTextStyle.headline24sp.copyWith(
+                  height: 1.25,
+                ),
+                textAlign: TextAlign.center,
               ),
-              textAlign: TextAlign.center,
             ),
             verticalSpacing(24),
             const AddExaminationButton()
